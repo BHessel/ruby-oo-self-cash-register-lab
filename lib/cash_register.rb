55e762a8fb2623e5d6c68955dc3fdbb566d381fa
BHessel <hesselfitness@gmail.com>
2020-10-08 13:03:27 -0400

    scott taking over

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
+
+
+class CashRegister
+    
+    #attr_accessor = :total
+    
+    def initialize(discount=nil) 
+        @total = 0
+        @discount = discount
+    end
+   
+    def cash_register
+    end
+
+    def cash_register_with_discount
+    end
+
+
+    def discount
+        @discount
+    end
+    
+    def total=(current_total)
+        @total = current_total
+    end
+    
+    def total(quantity=0.0)
+        @total
+    end
+    
+    def add_item (item, price, quantity)
+    #binding.pry
+        @total += price
+    end
+
+    
+
+
+
+
+
+end
 
